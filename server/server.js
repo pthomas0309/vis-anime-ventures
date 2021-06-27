cb74927be4837807c3aa0aa68b64c379cdbc015d
pthomas0309 <preston.thomas355@gmail.com>
2021-06-26 19:02:17 -0500

    server setup

---
+++ server/server.js
@@ -0,0 +1,26 @@
+// bring in express
+const express = require('express');
+
+// make express available as app
+const app = express();
+
+// bring in body parser
+const bodyParser = require('body-parser');
+
+// bring in the characters router
+const charactersRouter = require('./routes/characters.router');
+
+// declare PORT
+const port = process.env.PORT || 5000;
+
+/** ---------- MIDDLEWARE ---------- **/
+pp.use(bodyParser.json());
+app.use(express.static('build'));
+
+/** ---------- ROUTES ---------- **/
+app.use('/api/characters', charactersRouter);
+
+/** ---------- START SERVER ---------- **/
+app.listen(port, function () {
+    console.log('Listening on port: ', port);
+});
