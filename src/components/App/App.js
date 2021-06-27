ddebff8c60e1e93e91630ae51b7cf89ce2cd7d19
pthomas0309 <preston.thomas355@gmail.com>
2021-06-27 01:44:12 -0500

    create graph component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import CharacterData from '../CharacterData/CharacterData';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <CharacterData />
       </header>
     </div>
   );
---
+++ src/components/CharacterData/CharacterData.jsx
@@ -0,0 +1,5 @@
+export default function CharacterData() {
+    return (
+        <p>graphs</p>
+    )
+}
